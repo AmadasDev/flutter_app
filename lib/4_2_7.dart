9a3d24caa41bd84eef49d91382fd8ffcddb3d201
AmadasDev <amadas1004@gmail.com>
2021-07-27 16:15:21 +0900

    Chapter 4.2.7 GridView

---
+++ lib/4_2_7.dart
@@ -0,0 +1,57 @@
+//  Chapter 4.2.7 GridView
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('제목'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,  //  열 수
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+            ),
+            Container(
+              color: Colors.green,
+              width: 100,
+              height: 100,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+            ),
+          ],
+        ),
+    );
+  }
+}
