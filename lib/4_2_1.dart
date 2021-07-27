b1a879c88315c25ebbc95423c2e77387359337f1
AmadasDev <amadas1004@gmail.com>
2021-07-27 11:49:55 +0900

    Chapter 4.2.1 Container

---
+++ lib/4_2_1.dart
@@ -0,0 +1,38 @@
+//  Chapter 4.2.1 Container
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('제목'),
+      ),
+      body: Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
+      )
+    );
+  }
+}
