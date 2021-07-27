5e7b43053ee35e56229a06d1a61059622a542e02
AmadasDev <amadas1004@gmail.com>
2021-07-27 16:25:43 +0900

    Chapter 4.2.2 Column

--- lib/4_2_2.dart
+++ lib/4_2_2.dart
@@ -24,7 +24,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('제목'),
+          title: Text('Column'),
         ),
         body: Column(
           children: <Widget>[
