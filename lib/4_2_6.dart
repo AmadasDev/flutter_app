f8f66f23b8a11c2e21842c046c43d8b5774e4379
AmadasDev <amadas1004@gmail.com>
2021-07-27 15:59:19 +0900

    Chapter 4.2.5 ListView, ListTile

---
+++ lib/4_2_6.dart
@@ -0,0 +1,55 @@
+//  Chapter 4.2.6 ListView, ListTile
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  final items = List.generate(100, (i) => i).toList();  //  0부터 99까지의 값을 갖는 리스트 생성
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('제목'),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.event),
+            title: Text('Event'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text('Camera'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: () {},
+          ),
+        ],
+      )
+    );
+  }
+}
