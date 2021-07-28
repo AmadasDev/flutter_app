3e5a62356f27eca3bc6cab468eeba6859f131bb0
AmadasDev <amadas1004@gmail.com>
2021-07-28 16:38:47 +0900

    Chapter 4.4.1 ElevatedButton

---
+++ lib/4_4_1.dart
@@ -0,0 +1,37 @@
+//  Chapter 4.4.1 ElevatedButton
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ElevatedButton'),
+      ),
+      body: ElevatedButton(
+        child: Text('RaiseButton'),
+        onPressed: () {
+          //  실행될 코드 작성
+        },
+      )
+    );
+  }
+}
