ab07dc3fd3d676963643afef9f3db80ed3f13c7c
AmadasDev <amadas1004@gmail.com>
2021-07-28 16:48:17 +0900

    Chapter 4.4.3 IconButton

--- lib/4_4_3.dart
+++ lib/4_4_3.dart
@@ -24,7 +24,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('IconButton '),
+          title: Text('IconButton'),
         ),
         body: IconButton(
           icon: Icon(Icons.add),
