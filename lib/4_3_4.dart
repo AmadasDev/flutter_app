da6eb8486f9aaa5d845b135cc450a5188e4611c4
AmadasDev <amadas1004@gmail.com>
2021-07-28 16:06:39 +0900

    Chapter 4.3.4 Expanded

---
+++ lib/4_3_4.dart
@@ -0,0 +1,51 @@
+//  Chapter 4.3.4 Expanded
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Align'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,  //  기본값은 1
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
