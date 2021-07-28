67cd6162995b268b22e38cd7cf170d9fdaf6e68a
AmadasDev <amadas1004@gmail.com>
2021-07-28 16:18:10 +0900

    Chapter 4.3.5 SizedBox

---
+++ lib/4_3_5.dart
@@ -0,0 +1,38 @@
+//  Chapter 4.3.5 SizedBox
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('SizedBox'),
+        ),
+        body: SizedBox(
+          width: 100,
+          height: 100,
+          child: Container(
+            color: Colors.red,
+          ),
+        )
+    );
+  }
+}
