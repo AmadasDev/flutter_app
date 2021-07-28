7a31b6c771699709a9ed9845d680f09126bdfb79
AmadasDev <amadas1004@gmail.com>
2021-07-28 17:44:39 +0900

    Chapter 4.5.5 CircleAvatar

---
+++ lib/4_5_5.dart
@@ -0,0 +1,33 @@
+//  Chapter 4.5.5 CircleAvatar
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CircleAvatar'),
+      ),
+      body: CircleAvatar(
+        child: Icon(Icons.person),
+      )
+    );
+  }
+}
