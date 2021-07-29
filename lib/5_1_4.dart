7f86cf588cff18b4b42c143732452edfb554d1c5
AmadasDev <amadas1004@gmail.com>
2021-07-29 15:38:50 +0900

    Chapter 5.1.4 DropdownButton

---
+++ lib/5_1_4.dart
@@ -0,0 +1,55 @@
+//  Chapter 5.1.4 DropdownButton
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _valueList = ['첫 번째', '두 번째', '세 번째'];
+  var _selectedValue = '첫 번째';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DropdownButton'),
+      ),
+      body: DropdownButton(
+        value: _selectedValue,
+        items: _valueList.map(
+              (value) {
+            return DropdownMenuItem(
+              value: value,
+              child: Text(vale),
+            );
+          },
+        ).toList(),
+        onChanged: (value) {
+          setState(() {
+            _selectedValue = value;
+          });
+        },
+      ),
+    );
+  }
+}
