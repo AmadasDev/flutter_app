4fd5163829a9c35b53d9f65e80876c5ebf20fe10
AmadasDev <amadas1004@gmail.com>
2021-07-29 10:11:21 +0900

    Chapter 5.1.1 TextField

---
+++ lib/5_1_1.dart
@@ -0,0 +1,35 @@
+//  Chapter 5.1.1 TextField
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+//  여기까지는 공통 코드이다.
+
+//  여기부터 수정한다.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TextField'),
+      ),
+      body: TextField(
+        decoration: InputDecoration(
+          labelText: '여기에 입력하세요.',  //  힌트
+        ),
+      )
+    );
+  }
+}
