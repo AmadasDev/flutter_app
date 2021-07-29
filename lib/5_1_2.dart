2d35d8f57f16a59db9b03af8cd3167968065a766
AmadasDev <amadas1004@gmail.com>
2021-07-29 11:25:33 +0900

    Chapter 5.1.2 CheckBox와 Switch

--- lib/5_1_2.dart
+++ lib/5_1_2.dart
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Checkbox / Raido / Switch'),
+        title: Text('Checkbox / Radio / Switch'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
